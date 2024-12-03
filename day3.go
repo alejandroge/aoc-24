caa841b533be6826c9348516fd6d27a96ea875c4
Alejandro Guevara <alejandro.guevara.esc@gmail.com>
2024-12-03 20:18:30 +0100

    third day, second part

--- day3.go
+++ day3.go
@@ -33,5 +33,46 @@ func day3Part1() int {
 	return sum
 }
 
-func day3Part2() {
+func day3Part2() int {
+	lines := readFileLines("./inputs/day3.input.txt")
+
+	// 8707977 too low
+	pattern := `mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\)`
+	r, err := regexp.Compile(pattern)
+	if err != nil {
+		fmt.Println("whoopsie")
+		return 0
+	}
+
+	numberOfMatches := 0
+	enabled := true
+	sum := 0
+
+	enable_command := "do()"
+	disable_command := "don't()"
+
+	for _, line := range(lines) {
+		matches := r.FindAllStringSubmatch(line, -1)
+
+		numberOfMatches += len(matches)
+		for i := 0; i < len(matches); i++ {
+			match := matches[i]
+
+			switch match[0] {
+			case enable_command:
+				enabled = true
+			case disable_command:
+				enabled = false
+			default:
+				if enabled {
+					matchResult := stringToInt(match[1]) * stringToInt(match[2])
+					sum += matchResult
+				}
+			}
+		}
+	}
+
+	fmt.Println("total matches:", numberOfMatches)
+	fmt.Println("total sum:", sum)
+	return sum
 }
